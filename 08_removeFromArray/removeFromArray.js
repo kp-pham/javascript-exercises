f6c521aa4f8f668e87f3e0f7a66efc3e2b461c93
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 10:58:40 -0700

    feat: remove all occurrences of key
    Loop through array and use splice method to remove element.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, key) {
+    for (let i = 0; i < array.length; ++i) {
+        if (array[i] == key) {
+            array.splice(i, 1);
+            --i;
+        }
+    }
 };
 
 // Do not edit below this line
