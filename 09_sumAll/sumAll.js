48b464535a6e17d2e952f3285d5a8113dc80fd4c
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:27:56 -0700

    refactor: remove .skip from second test case
    Pass test case for summing numbers within range.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,4 @@
-const { useReducer } = require("react");
+// const { useReducer } = require("react");
 
 const sumAll = function(start, end) {
     const range = Array.from({ length: end - start + 1 }, (_, i) => start + i);
