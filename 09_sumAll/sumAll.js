bafafa0ea6ae843c27c0e2e5ffaf49a4745f2f38
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:20:25 -0700

    feat: construct array from range of numbers
    Create array from array-like object with function expression.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
+const { useReducer } = require("react");
 
+const sumAll = function(start, end) {
+    const range = Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    console.log(range);
 };
 
 // Do not edit below this line
