8431f071f18ab0c75162124c9cef18de16d9213d
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 12:46:32 -0700

    refactor: add alternative solution
    Use toFixed and parseFloat when displaying and reading from user input.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function(farenheit) {
-  celsius = (farenheit - 32) * (5 / 9);
-  return parseFloat(celsius.toFixed(1));
+  // celsius = (farenheit - 32) * (5 / 9);
+  // return parseFloat(celsius.toFixed(1));
+  return Math.round((farenheit - 32) * (5 / 9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
-  farenheit = celsius * (9 / 5) + 32;
-  return parseFloat(farenheit.toFixed(1));
+  // farenheit = celsius * (9 / 5) + 32;
+  // return parseFloat(farenheit.toFixed(1));
+  return Math.round(((celsius* (9 / 5) + 32) * 10)) / 10;
 };
 
 // Do not edit below this line
