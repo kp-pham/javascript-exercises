919a75db4962d035780d28b728cf6807312605b4
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:32:57 -0700

    refactor: return ERROR when start and end are negative
    Ensure no negative numbers are allowed in the range.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 // const { useReducer } = require("react");
 
 const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return "ERROR";
     if (start > end) [start, end] = [end, start];
 
     const range = Array.from({ length: end - start + 1 }, (_, i) => start + i);
