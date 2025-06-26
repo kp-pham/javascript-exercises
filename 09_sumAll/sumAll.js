f05adeb47b91fc1c013aeb68e0c1947e282095bc
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:30:04 -0700

    refactor: swap start and end
    Ensure end is greater than start before generating the range.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 // const { useReducer } = require("react");
 
 const sumAll = function(start, end) {
+    if (start > end) [start, end] = [end, start];
+
     const range = Array.from({ length: end - start + 1 }, (_, i) => start + i);
     return range.reduce((sum, value) => sum + value, 0);
 };
