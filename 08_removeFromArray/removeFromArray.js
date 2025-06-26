f80e44509d33a728bff464cc1fe2d34e0b66deae
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:00:34 -0700

    refactor: return original array
    Function calls evaluate to the return value when used in an expression.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
 const removeFromArray = function(array, key) {
     for (let i = 0; i < array.length; ++i) {
-        if (array[i] == key) {
+        if (array[i] === key) {
             array.splice(i, 1);
             --i;
         }
     }
+
+    return array;
 };
 
 // Do not edit below this line
