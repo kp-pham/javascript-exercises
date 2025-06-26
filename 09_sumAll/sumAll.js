0a3b67213d3b4f50044a2b046ec3ecc7aa4e296f
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-26 11:22:40 -0700

    feat: use reduce method to sum values
    Use accumulator to store the sum of the current value and previous values.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const { useReducer } = require("react");
 
 const sumAll = function(start, end) {
     const range = Array.from({ length: end - start + 1 }, (_, i) => start + i);
-    console.log(range);
+    return range.reduce((sum, value) => sum + value, 0);
 };
 
 // Do not edit below this line
