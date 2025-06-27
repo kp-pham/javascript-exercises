44a3a4d94801d16ef1edd1e4edf794df8b1aedc3
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 16:43:10 -0700

    feat: implement power function
    Use expoentiation operator to return the base raised to the specified power.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(numbers) {
   return numbers.reduce((accumulator, number) => accumulator * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 const factorial = function() {
