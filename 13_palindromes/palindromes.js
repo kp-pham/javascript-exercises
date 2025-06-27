a6778559c2083b2f637cb12da20a3c69b9dc4388
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 16:58:12 -0700

    feat: implement two pointers approach
    Use two points for left and right of string to see if characters match.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let left = 0;
+    let right = string.length - 1;
 
+    while (left < right) {
+        if (string[left] != string[right]) return false;
+
+        ++left;
+        --right;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
