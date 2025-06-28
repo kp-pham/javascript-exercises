f1333d108b8856824a52cdfd7c366425418fdb2a
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 17:44:08 -0700

    feat: use iteration to generate Fibonacci sequence
    Use two variables to store the current number and previous number.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {    
+    let a = 0;
+    let b = 1;
 
+    for (let i = 0; i > n; ++i) {
+        [b, a] = [b += b + a, b];
+    }
+
+    return b;
 };
 
 // Do not edit below this line
