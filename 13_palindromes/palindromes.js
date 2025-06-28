d021c0c15238dd387341ca549b1aca6cbbde2a78
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 17:11:35 -0700

    refactor: implement character encoding and checking for alpnumeric characters
    Exclude nonalphanumeric characters such as puncutation and whitespace.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -3,14 +3,34 @@ const palindromes = function (string) {
     let right = string.length - 1;
 
     while (left < right) {
-        if (string[left] != string[right]) return false;
+        let leftCharCode = getCharCode(string, left);
+        let rightCharCode = getCharCode(string, right);
 
-        ++left;
-        --right;
+        if (!isAlphaNumeric(leftCharCode) || !isAlphaNumeric(rightCharCode)) {
+            if (!isAlphaNumeric(leftCharCode))
+                ++left;
+
+            if (!isAlphaNumeric(rightCharCode))
+                --right;
+        }
+        else if (string[left] != string[right]) 
+            return false;
+        else {
+            ++left;
+            --right;
+        }
     }
 
     return true;
 };
+
+function getCharCode(string, index) {
+    return string.charCodeAt(index);
+}
+
+function isAlphaNumeric(charCode) {
+    return 'a' <= charCode && charCode <= 'z';
+}
 
 // Do not edit below this line
 module.exports = palindromes;
