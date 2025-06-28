68600fa8dc90b6557586c06ba059e92b53b0d215
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-27 17:51:35 -0700

    refactor: account for cases when n is 0 or 1
    Start iteration when base cases are not satisfied.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,9 +1,14 @@
 const fibonacci = function(n) {    
-    let a = 0;
+    if (n === 0) return 0;
+    else if (n === 1) return 1;
+
+    let a = 1;
     let b = 1;
 
-    for (let i = 0; i > n; ++i) {
-        [b, a] = [b += b + a, b];
+    for (let i = 2; i < n; ++i) {
+        let c = a + b;
+        a = b;
+        b = c;
     }
 
     return b;
